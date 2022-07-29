76db72627a9873f005a56eb7dab9a589391159f7
Jesús Álvarez <khisus19@gmail.com>
2022-07-29 16:16:56 -0500

    Buttons ready

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import Boton from './components/Boton';
 import freeCodeCampLogo from './img/fcc_primary_large.svg';
 
 function App() {
+
+  const manejarClic = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className='App'>
       <div className='freecodecamp-logo-container'>
@@ -11,7 +21,14 @@ function App() {
           alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-principal'>
-        
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
       </div>
     </div>
   );
