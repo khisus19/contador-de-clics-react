5697685adb9059dd655892bbbd12123b50ca61d5
Jesús Álvarez <khisus19@gmail.com>
2022-07-29 17:13:53 -0500

    Counter finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Boton from './components/Boton';
+import Contador from './components/Contador';
 import freeCodeCampLogo from './img/fcc_primary_large.svg';
+import { useState } from 'react';
 
 function App() {
 
+  const [numClics, setNumClics] = useState(0);
+
   const manejarClic = () => {
-    console.log('Clic');
+    setNumClics(numClics + 1);
   }
 
   const reiniciarContador = () => {
-    console.log('Reiniciar');
+    setNumClics(0);
   }
 
   return (
@@ -21,6 +25,7 @@ function App() {
           alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
         <Boton
           texto='Clic'
           esBotonDeClic={true}
